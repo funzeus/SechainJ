7ca947c6f328808a12a75f6e8531720b742dbdbe
park <desjin2002@naver.com>
2016-11-08 20:23:05 +0900

    Simple tx to JSON

--- sechianJ/src/mainController/Main.java
+++ sechianJ/src/mainController/Main.java
@@ -1,13 +1,34 @@
 package mainController;
 
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import crypto.KeyGenerator;
+import transaction.Transaction;
 
 public class Main {
+	
+	static JSONObject txJSON = new JSONObject();
+	
 	public static void main(String args[]){
 		
 		System.out.println("Start");
-//		KeyGenerator.getKeyGen();
+		
+		KeyGenerator.getKeyGen();
+		Transaction tx = new Transaction(1, "transaction input", "transaction output");
+		
+		try {
+			
+			txJSON = Transaction.txToJSON(tx);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String jsonTx = txJSON.toString();
+		
+		System.out.println(jsonTx);
 	}
 }
 
--- sechianJ/src/transaction/Transaction.java
+++ sechianJ/src/transaction/Transaction.java
@@ -1,21 +1,35 @@
 package transaction;
 
+import org.json.*;
+
 public class Transaction {
 
 
+	/*
+	 * To be added 
+	 * 			Timestamp 
+	 * 			sender ip address
+	 * 			receiver ip address 
+	 */
+	
 	private int txType;
-	private byte[] txInput;
-	private byte[] txOutput;
-//	public enum transactionType{
-//		NORMAL, SMART_CONTRACT, TEST
-//	}
-	
-	
-	public Transaction(final int txType, byte[] txInput, byte[] txOutput){
+	private String txInput;
+	private String txOutput;
+
+	public Transaction(final int txType, String txInput, String txOutput){
 		this.txType = txType;
 		this.txInput = txInput;
 		this.txOutput = txOutput;
 		
+	}
+	
+	public static JSONObject txToJSON(Transaction tx) throws JSONException{
+		JSONObject txJSON = new JSONObject();
+		txJSON.put("txType", tx.txType);
+		txJSON.put("txInput", tx.txInput);
+		txJSON.put("txOutput", tx.txOutput);
+		
+		return txJSON; 
 	}
 	
 	
